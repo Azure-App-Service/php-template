8b909ebd72d91891c7c12993f7fc76527e0e6df2
Patrick Lee <31744877+patricklee2@users.noreply.github.com>
2019-10-23 15:29:46 -0700

    banner

--- template-apache/init_container.sh
+++ template-apache/init_container.sh
@@ -11,6 +11,7 @@ A P P   S E R V I C E   O N   L I N U X
 Documentation: http://aka.ms/webapp-linux
 PHP quickstart: https://aka.ms/php-qs
 PHP version : `php -v | head -n 1 | cut -d ' ' -f 2`
+Note: Any data outside '/home' is not persisted
 EOL
 cat /etc/motd
 
